a58d6e6f937377604a098602007be287ba7a4b85
rahmonovb0407-dotcom <rahmonovb0407@gmail.com>
2026-07-06 08:52:25 +0500

    Delete channel.py

--- channel.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class ChannelStates(StatesGroup):
-    # Majburiy kanal qo'shish holati
-    waiting_for_channel_id = State()
-    # Majburiy kanal o'chirish holati
-    waiting_for_delete_channel = State()
